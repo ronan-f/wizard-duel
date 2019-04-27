08caed55f53cbf4216c488eddc3d933bb42a5c5c
kcdporter <kiracdporter@gmail.com>
2019-04-27 18:44:44 +0000

    adding first commit and updating knexfile.js to pg db

--- express-back-end/knexfile.js
+++ express-back-end/knexfile.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 module.exports = {
 
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL,
+      database : "wizard_duel",
+      user     : "development",
+      password : "development",
+      hostname : 'localhost',
+      port     : 3000,
+      ssl      : true,
     },
     migrations: {
       directory: './db/migrations',
