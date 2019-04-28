4cfc2edf3f4e74ae42300ac074e13b9feaf39417
kcdporter <kiracdporter@gmail.com>
2019-04-28 04:15:00 +0000

    updating packages for react-router

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -6,34 +6,10 @@ import Instructions from './Instructions';
 import Navigation from './Navigation.js';
 
 
-<<<<<<< HEAD
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
-
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    })
-  }
-=======
->>>>>>> 37023f99cf47da26fcfab21e567a167ea4a0eb60
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>
-      </div>
-    );
-=======
       <BrowserRouter>
         <div>
           <Navigation />
@@ -42,7 +18,6 @@ class App extends Component {
         </div>
       </BrowserRouter>
     )
->>>>>>> 37023f99cf47da26fcfab21e567a167ea4a0eb60
   }
 }
 
