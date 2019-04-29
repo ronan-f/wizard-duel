a3d2c4d6cdc87fd3f61e57ad3c74642df16be4d1
kcdporter <kiracdporter@gmail.com>
2019-04-29 22:13:56 +0000

    adding the wizards and spells objects to set state from db to react

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -8,17 +8,30 @@ App.use(BodyParser.urlencoded({ extended: false }));
 App.use(Express.static('public'));
 
 
-App.get('/api/data', (req, res) => {
+App.get('/api/wizards', (req, res) => {
   let response = function(characters) {
     let wizards = {wizards:characters}
-    console.log("THESE WIZZES YO", wizards)
+    console.log("THE WIZARDS:", wizards)
     res.json({
-      message:`And then there was ${voldy} and ${draco}`
-      //message: query[0].name
+      message:`Coming back from the server with the wizards ${wizards}`,
+      wizards
     });
   }
   knex.select('*').from('characters')
   .then(characters => response(characters));
+});
+
+App.get('/api/spells', (req, res) => {
+  let response = function(thespells) {
+    let spells = {spells:thespells}
+    console.log("THE SPELLS:", spells)
+    res.json({
+      message:`Coming back from the server with the spells ${spells}`,
+      spells
+    });
+  }
+  knex.select('*').from('spells')
+  .then(thespells => response(thespells));
 });
 
 App.listen(PORT, () => {
