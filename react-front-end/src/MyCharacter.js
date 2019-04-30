cac14b404a6713907cf4df5743b98f18ed8150bc
kcdporter <kiracdporter@gmail.com>
2019-04-30 22:09:39 +0000

    editing backgrounds and layout

--- react-front-end/src/MyCharacter.js
+++ react-front-end/src/MyCharacter.js
@@ -6,9 +6,18 @@ class MyCharacter extends Component {
     render() {
         const { characterInfo } = this.props
         return(
-            <div>
+            <div className='characterSelection'>
+            <div className='playerDiv'>
+                {/* <img className='playerFrog' src='/frog_card_template.png' alt='my character'/>   */}
                 <img className='myChar' src='/character_gifs/right_view/ron.gif' alt='my character'/>
+            </div>
+            <div className='playerDiv'>
+                {/* <img className='myChar' src='/character_gifs/right_view/ron.gif' alt='my character'/> */}
+            </div>
+            <div className='opponentDiv'>
+                {/* <img className='opponentFrog' src='/frog_card_template.png' alt='my character'/> */}
                 <h4 className='health'>My Defence: {characterInfo.health}</h4>
+            </div>
             </div>
         )
     }
