4c03ea061db5fa4b3ca6aa342c7f84129005dc2c
Ronan Fegan <rfegan13@gmail.com>
2019-05-01 02:54:47 +0000

    Passing props to login

--- react-front-end/src/Game.js
+++ react-front-end/src/Game.js
@@ -9,39 +9,6 @@ import axios from 'axios';
 
 
 class Game extends Component {
-    constructor(props) {
-      super(props)
-      this.chooseSpell = this.chooseSpell.bind(this);
-      this.state = {
-        currentSpell: '',
-        spells:
-        [
-          {
-            id: 1,
-            name: 'Expelliarmus',
-            description: 'Blast your opponent',
-            power: 5,
-            limit: 3
-          },
-          {
-            id: 2,
-            name: 'avadakadabra',
-            description: 'kills opponent',
-            power: 10,
-            limit: 1
-          }
-        ],
-        notifications: ['New player has joined', 'Player 1, your turn!'],
-        myCharacter: {name: 'Dumbledore', image: 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2f/101-albus_dumbledore.gif/revision/latest/scale-to-width-down/180?cb=20120622181924', health: 10},
-        opponentCharacter: {name: 'Ron', image:'https://vignette.wikia.nocookie.net/harrypotter/images/2/2f/101-albus_dumbledore.gif/revision/latest/scale-to-width-down/180?cb=20120622181924' }
-
-      }
-    }
-
-    chooseSpell(spell){
-      this.setState({currentSpell: spell})
-    }
-
     fetchData = () => {
       axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
       .then((response) => {
@@ -56,11 +23,12 @@ class Game extends Component {
     }
 
     render() {
-      const { spells, notifications, myCharacter, opponentCharacter, currentSpell } =  this.state
+      console.log(this.props.state)
+      const { spells, notifications, myCharacter, opponentCharacter } =  this.props.state
       return (
         <div className="App">
           <div className='infoBar'>
-            < PlayerSpellList chooseSpell={this.chooseSpell} userSpells={spells}/>
+            < PlayerSpellList chooseSpell={this.props.chooseSpell} userSpells={spells}/>
             < NotificationBar notifications={notifications} />
           </div>
 
@@ -69,12 +37,12 @@ class Game extends Component {
             < MyCharacter characterInfo={myCharacter} />
           </div>
 
-          <button className='castSpellBtn' onClick={() => this.setState({notifications: notifications.concat(`Player 1 used ${currentSpell}`)})}>
+          <button className='castSpellBtn' onClick={() => this.props.newNotification()}>
               Cast Spell
           </button>
 
           <div>
-            <h1>{ this.state.message }</h1>
+            <h1>{ this.props.state.message }</h1>
             <button onClick={this.fetchData} >
               Fetch Data
             </button>
--- react-front-end/src/Login.js
+++ react-front-end/src/Login.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './login.css'
+import Setup from './Setup.js';
 
 class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: '',
+            redirect: false
+        }
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.newUser(this.state.user)
+        this.setState({redirect: true});
+    }
     render() {
         return (
+        this.state.redirect ? <Setup state={this.props.state}/> :
             <div>
-                <h2 className='welcomeMessage'>PROPS {this.props.test}</h2>
-                <form className='loginForm'>
+                <form className='loginForm' onSubmit={this.handleSubmit.bind(this)}>
                     <label>
                         Name:
-                        <input type="text" name="name" />
+                        <input type="text" name="name" onChange={(event) => this.setState({user: event.target.value})} />
                     </label>
                     <input type="submit" value="Alohomora" />
                 </form>
