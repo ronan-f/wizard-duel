f5d93e4af814082d00d0964f37b8c133660b2fbd
kcdporter <kiracdporter@gmail.com>
2019-05-07 21:30:16 -0700

    user and leveling working

--- express-back-end/seeds/users.js
+++ express-back-end/seeds/users.js
@@ -4,11 +4,11 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, username: 'Sebastian', email: 'seb@seb.ca', password:'', experience_points:0, wins: 0, losses:0 },
+        {id: 1, username: 'sebastian', email: 'seb@seb.ca', password:'', experience_points:800, wins: 0, losses:0 },
 
-        {id: 2, username: 'Ronan', email: 'ronan@ronan.ca', password:'lighthouse', experience_points:0, wins: 1, losses:1 },
+        {id: 2, username: 'ronan', email: 'ronan@ronan.ca', password:'lighthouse', experience_points:0, wins: 1, losses:1 },
 
-        {id: 3, username: 'Kira', email: 'kira@kira.ca', password:'Yukon', experience_points:0, wins: 2, losses:2 },
+        {id: 3, username: 'kira', email: 'kira@kira.ca', password:'Yukon', experience_points:0, wins: 2, losses:2 },
       ]);
     });
 };
